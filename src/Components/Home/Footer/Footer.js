3c75f575c14716b3c3890546cf9df41a476492d9
unknown <iarafat386@gmail.com>
2021-06-19 16:32:58 +0600

    Footer updated, Teachers Image & Name Updated

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -3,33 +3,42 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <section className="bg-secondary">
-            <section className="container">
+        <section className="Footer Font1">
+            <section className="container text-white">
                 <div className="row pt-5 pb-5">
-                <div className="col-md-4 text-center">
+                    <div className="col-md-3 text-center mt-3">
                         <p>Principal</p>
                         <p>Notice</p>
                         <p>Teachers</p>
+                        <p>Result</p>
                         <p>email@example.com</p>
                     </div>
-                    <div className="col-md-4 text-center">
+                    <div className="col-md-3 text-center mt-3">
                         <p>Class 6</p>
                         <p>Class 7</p>
                         <p>Class 8</p>
                         <p>Class 9</p>
                         <p>Class 10</p>
                     </div>
-                    <div className="col-md-4 text-center">
-                        <p>Shimpur High School, <br /> Shimpur Comilla.</p>
+                    <div className="col-md-3 text-center mt-3">
+                        <p>Shimpur High School, <br /> <br /> Shimpur Comilla.</p>
+                        <p>Location</p>
+                        <p>Help</p>
+                    </div>
+                    <div className="col-md-3 text-center mt-3">
+                        <p>Facebook</p>
+                        <p>LinkdIn</p>
+                        <p>Twitter</p>
+                        <p>Youtube</p>
                     </div>
                 </div>
             </section>
             <div className="container">
-            <div className="row">
-                <div className="col-md-12 text-center text-primary">
-                    <p>Developed by <b>"Imtious Islam Midul"</b></p>
+                <div className="row">
+                    <div className="col-md-12 text-center text-secondary">
+                        <p>Developed by <b>"Imtious Islam Midul"</b></p>
+                    </div>
                 </div>
-            </div>
             </div>
         </section>
     );
--- src/Components/Home/Teachers/Teachers.js
+++ src/Components/Home/Teachers/Teachers.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Teachers.css';
 import T1 from '../../../Images/IMG_20210125_200350-02-removebg-preview.png';
+import T2 from './download.jpg';
+import T3 from './download.png';
+import T4 from './images.jpg';
+import T5 from './images (1).jpg';
+import T6 from './images.png';
 
 const Teachers = () => {
     return (
@@ -18,32 +23,32 @@ const Teachers = () => {
                     </div>
                     <div className="col-md-4">
                         <div className="Teachers--Card mb-5">
-                            <img className="Teachers--Image" src={T1} alt="" />
-                            <p className="Teachers--Name">Mrs. Lina</p>
+                            <img className="Teachers--Image" src={T2} alt="" />
+                            <p className="Teachers--Name">Mr. Alan</p>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="Teachers--Card mb-5">
-                            <img className="Teachers--Image" src={T1} alt="" />
-                            <p className="Teachers--Name">Mrs. Lina</p>
+                            <img className="Teachers--Image" src={T3} alt="" />
+                            <p className="Teachers--Name">Mr. Rian</p>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="Teachers--Card mb-5">
-                            <img className="Teachers--Image" src={T1} alt="" />
-                            <p className="Teachers--Name">Mrs. Lina</p>
+                            <img className="Teachers--Image" src={T4} alt="" />
+                            <p className="Teachers--Name">Mrs. Nomito</p>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="Teachers--Card mb-5">
-                            <img className="Teachers--Image" src={T1} alt="" />
-                            <p className="Teachers--Name">Mrs. Lina</p>
+                            <img className="Teachers--Image" src={T5} alt="" />
+                            <p className="Teachers--Name">Mr. Harry</p>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="Teachers--Card mb-5">
-                            <img className="Teachers--Image" src={T1} alt="" />
-                            <p className="Teachers--Name">Mrs. Lina</p>
+                            <img className="Teachers--Image" src={T6} alt="" />
+                            <p className="Teachers--Name">Mrs. Suzen</p>
                         </div>
                     </div>
                 </div>
